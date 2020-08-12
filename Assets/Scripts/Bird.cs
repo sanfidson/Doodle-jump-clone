54da83dc5f0987556df22e76f88b150ccd3bad95
sanfidson <44056950+sanfidson@users.noreply.github.com>
2020-08-12 19:12:48 +0300

    Add files via upload
    First commit.

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public Rigidbody2D rigitbody;
+    public float jumpForce;
+    public float speed;
+
+    private void Update() 
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+    
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigitbody.velocity = new Vector2(0, 0);
+        rigitbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+    }
+
+}
